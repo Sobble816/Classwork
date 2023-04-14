effb2f0b08c0202c15c8d5ce804eb82a7b8b3c3d
Sobble816 <124954983+Sobble816@users.noreply.github.com>
2023-04-14 10:42:03 -0400

    Update minigame.py

--- PygameWorks/minigame.py
+++ PygameWorks/minigame.py
@@ -20,11 +20,19 @@ clock = pygame.time.Clock()
 # -------------------
 # Initialize global variables
 
+#start button
+start_x = 300
+start_y = 300
+start_radius = 100
+
+
+game = 0
+
 frame_count = 0
-
-
 score = 0
-score_font = pygame.font.SysFont("Comic Sans MS", 30)
+timer = 500
+
+font = pygame.font.SysFont("Comic Sans MS", 30)
 
 
 circle_radius = 50
@@ -49,63 +57,92 @@ while running:
         elif event.type == MOUSEBUTTONDOWN:
             
             mouse_x, mouse_y = event.pos
-            a = target_x - mouse_x
-            b = target_y - mouse_y
+
+        #start button
+            a = start_x - mouse_x
+            b = start_y - mouse_y
             distance = math.sqrt(a**2 + b**2)
-            if distance < circle_radius:
+            if distance < start_radius:
+                game = 1
+                score = 0
+                timer = 500
+
+        #target
+            target_A = target_x - mouse_x
+            target_B = target_y - mouse_y
+            target_distance = math.sqrt(target_A**2 + target_B**2)
+            if target_distance < circle_radius:
                 score += 1
                 frame_count = 0
                 target_x = random.randrange(0, 600)
                 target_y = random.randrange(0, 600)
+    
+
+    if game == 1:
         
+    #target boundery
+        if target_x <= 150:
+            target_x = 150
+        elif  target_x <= 300:
+            target_x = 300
+        else:
+            target_x = 450
+        
+        if target_y <= 150:
+            target_y = 150
+        elif target_y <= 300:
+            target_y = 300
+        else:
+            target_y = 450  
+        
+        
+        frame_count += 1
+        timer -= 1
+        
+    #drawing board
+        screen.fill("white")
+        #collum 1
+        pygame.draw.circle(screen, "grey", (WIDTH/2, HEIGHT/2), circle_radius)
+        pygame.draw.circle(screen, "grey", (WIDTH/2, HEIGHT/4), circle_radius)
+        pygame.draw.circle(screen, "grey", (WIDTH/2, HEIGHT/1.33), circle_radius)
 
-    #target 1 boundery
-    if target_x <= 150:
-        target_x = 150
-    elif  target_x <= 300:
-        target_x = 300
-    else:
-        target_x = 450
-    
-    if target_y <= 150:
-        target_y = 150
-    elif target_y <= 300:
-        target_y = 300
-    else:
-        target_y = 450  
-    
-    
-    frame_count += 1
+        #collum 2
+        pygame.draw.circle(screen, "grey", (WIDTH/4, HEIGHT/2), circle_radius)
+        pygame.draw.circle(screen, "grey", (WIDTH/4, HEIGHT/4), circle_radius)
+        pygame.draw.circle(screen, "grey", (WIDTH/4, HEIGHT/1.33), circle_radius)
 
-    
-    
-    screen.fill("white")
-    #collum 1
-    pygame.draw.circle(screen, "grey", (WIDTH/2, HEIGHT/2), circle_radius)
-    pygame.draw.circle(screen, "grey", (WIDTH/2, HEIGHT/4), circle_radius)
-    pygame.draw.circle(screen, "grey", (WIDTH/2, HEIGHT/1.33), circle_radius)
-
-    #collum 2
-    pygame.draw.circle(screen, "grey", (WIDTH/4, HEIGHT/2), circle_radius)
-    pygame.draw.circle(screen, "grey", (WIDTH/4, HEIGHT/4), circle_radius)
-    pygame.draw.circle(screen, "grey", (WIDTH/4, HEIGHT/1.33), circle_radius)
-
-    #collum 3
-    pygame.draw.circle(screen, "grey", (WIDTH/1.33, HEIGHT/2), circle_radius)
-    pygame.draw.circle(screen, "grey", (WIDTH/1.33, HEIGHT/4), circle_radius)
-    pygame.draw.circle(screen, "grey", (WIDTH/1.33, HEIGHT/1.33), circle_radius)
+        #collum 3
+        pygame.draw.circle(screen, "grey", (WIDTH/1.33, HEIGHT/2), circle_radius)
+        pygame.draw.circle(screen, "grey", (WIDTH/1.33, HEIGHT/4), circle_radius)
+        pygame.draw.circle(screen, "grey", (WIDTH/1.33, HEIGHT/1.33), circle_radius)
 
 
 
     #drawing targets
-    if frame_count < 15:
-        pygame.draw.circle(screen, "blue", (target_x, target_y), circle_radius)
+        if frame_count < 15:
+            pygame.draw.circle(screen, "blue", (target_x, target_y), circle_radius)
 
 
 
-    score_text = score_font.render(f"Score: {score}", False, (0, 0, 0))
-    screen.blit(score_text, (0,0))
+        score_text = font.render(f"Score: {score}", False, (0, 0, 0))
+        screen.blit(score_text, (0,0))
+        
+        timer_text = font.render(f"Time: {timer}", False, (0, 0, 0))
+        screen.blit(timer_text, (450,0))
 
+        if timer == 0:
+            game = 0
+
+    else:
+    #start screen
+        screen.fill("white")
+        
+        score_text = font.render(f"Score: {score}", False, (0, 0, 0))
+        screen.blit(score_text, (0,0))
+
+        pygame.draw.circle(screen, "green", (start_x, start_y), start_radius)
+        button_text = font.render("START", False, (0, 0, 0))
+        screen.blit(button_text, (245, 278))
 
 
     pygame.display.flip()
